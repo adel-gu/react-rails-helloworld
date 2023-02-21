dbb1688e3606ca0a93a057c3412b82afc00d0a51
adelguit <adeelguitoun@gmail.com>
2023-02-21 12:00:03 +0100

    Create the app root

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,14 @@ import '@hotwired/turbo-rails';
 import './controllers';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+);
