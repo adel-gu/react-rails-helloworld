918885ee372e888745ff739106d8666e9f483b07
adelguit <adeelguitoun@gmail.com>
2023-02-21 11:59:39 +0100

    Set the App react route

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+// Routers
+import { Routes, Route } from 'react-router-dom';
+
+// Redux store
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+
+// Components
+import Message from './Message';
 
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<Message />} />
+      </Routes>
+    </Provider>
+  );
 };
 
 export default App;
